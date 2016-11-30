ba3decd413b541aeb65c02e5a44a1ad06b6a0b22
Tristan Brindle <t.c.brindle@gmail.com>
2016-11-30 14:34:03 +0000

    Add test case including all headers
    To make sure we have no conflicting definitions

---
+++ test/test_all_includes.cpp
@@ -0,0 +1,15 @@
+
+#define STX_NO_STD_ANY
+#include <stx/any.hpp>
+
+#define STX_NO_STD_OPTIONAL
+#include <stx/optional.hpp>
+
+#define STX_NO_STD_STRING_VIEW
+#include <stx/string_view.hpp>
+
+#define STX_NO_STD_VARIANT
+#include <stx/variant.hpp>
+
+int main()
+{}
